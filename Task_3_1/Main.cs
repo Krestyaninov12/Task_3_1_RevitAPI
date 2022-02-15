7d5eafa7ce08e5e252b59a304298e60bf3c6f16c
Krestyaninov12 <kreda@pnpe.ru>
2022-02-16 00:39:54 +0300

    Добавьте файлы проекта.

---
+++ Task_3_1/Main.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI.Selection;
+
+namespace Task_3_1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Element, new WallFilter(), "Выберите стены");
+           
+            double Sum = 0;
+
+            foreach (var selectedElement in selectedElementRefList)
+            {
+                Element element = doc.GetElement(selectedElement);
+                
+                Parameter volumeParameter = element.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED);
+                if (volumeParameter.StorageType == StorageType.Double)
+                {
+                    double volumeValue = UnitUtils.ConvertFromInternalUnits(volumeParameter.AsDouble(), UnitTypeId.CubicMeters);
+                    Sum += volumeValue;
+                }
+            }
+
+            TaskDialog.Show("Объем стен", $"Объем стен: {Sum} м³");
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Task_3_1/WallFilter.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_1
+{
+    internal class WallFilter : ISelectionFilter
+    {
+
+        public bool AllowElement(Element elem)
+        {
+            return elem is Wall;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
